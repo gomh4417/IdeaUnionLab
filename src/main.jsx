30c2473d26a31d8973d32ae4a61279f3043a1762
gmh <myard0118@naver.com>
2025-07-16 04:36:26 +0900

    Save file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
 import './index.css'
+
+import HomePage from './page/HomePage'
+import LabPage from './page/LabPage'
+import ResultPage from './page/ResultPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    
-    <>
-      <h1>안녕하세요</h1>
-    </>
-
-
+    <ThemeProvider theme={theme}>
+      <Router basename={import.meta.env.BASE_URL}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/lab" element={<LabPage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/page/LabPage.jsx
@@ -0,0 +1,5 @@
+function LabPage() {
+    return <h1>Lab Page</h1>
+}
+
+export default LabPage
