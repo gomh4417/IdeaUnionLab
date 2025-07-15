837edaa9364fc20313217560785e8d7d7d41df89
gmh <myard0118@naver.com>
2025-07-16 04:12:44 +0900

    Save file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    
+    <>
+      <h1>안녕하세요</h1>
+    </>
+
+
   </StrictMode>,
 )
